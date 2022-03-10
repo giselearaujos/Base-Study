b49a7ba612866959fabf41c3b5641188eae21864
giselearaujos <giselearaujo07.s@gmail.com>
2022-03-10 16:19:12 -0300

    :sparkles: Add javascript fiel

---
+++ Javascript/001-variáveis.js
@@ -0,0 +1,17 @@
+/* JavaScript é uma linguagem dinamicamente tipada.
+  Isso significa que você não precisa especificar o tipo de dado quando vai declarar uma variável.
+  O tipo de dado da variável é inferido automaticamente a partir do seu valor.
+*/
+
+// Declaração de variável
+
+let name; // Declara variável
+name = "Giza"; // Inicializa uma variável
+console.log(name); // Giza
+
+name = "Lana"; // Reatribuição
+console.log(name); // Lana
+
+// Não podemos redeclarar variáveis com let
+// let name = "Olíva";
+// SyntaxError: Identifier 'name' has already been declared
