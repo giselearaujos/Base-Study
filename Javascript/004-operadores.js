4475396f3753d247f3ed8982adc1d8c013621d05
giselearaujos <giselearaujo07.s@gmail.com>
2022-03-15 21:59:17 -0300

    :seedling: Add new file in javascript

---
+++ Javascript/004-operadores.js
@@ -0,0 +1,65 @@
+/*-------- Operadores ----------*/
+
+// => Operador de atribuição "="
+
+/*
+Seu funcionamento é bem simples, ele atribui o valor do elemento à direita
+ao elemento à sua esquerda. 
+O valor da direita pode vir de um valor literal
+(um número, uma string, etc.), de uma variável ou de uma expressão.
+*/
+const food = "Pizza";
+
+// => Operadores aritméticos
+
+// Adição (+), concatenar e tenta converter o operando em um número, sempre que possível.
+const num1 = 23;
+const num2 = 22;
+console.log(num1 + num2);
+
+const whatIsThis = "100";
+console.log(+whatIsThis);
+console.log("--------------------------------");
+
+// Subtração (-)
+const salary = 1000;
+const discount = 120;
+console.log(salary - discount);
+console.log("--------------------------------");
+
+// Multiplicação (*)
+const fees = 20;
+console.log(salary * fees);
+console.log("--------------------------------");
+
+// Divisão (/)
+const division = 5;
+console.log(salary / division);
+console.log("--------------------------------");
+
+// Módulo (%)
+const rest = 2;
+console.log(salary % rest);
+console.log("--------------------------------");
+
+// Incremento (++)
+let steps = 0;
+console.log(++steps);
+console.log("--------------------------------");
+
+// Decremento (--)
+let points = 10;
+console.log(--points);
+console.log("--------------------------------");
+
+// Negação (-) Operador unário. Retorna a negação de seu operando.
+const negative = 3;
+console.log(-negative);
+console.log("--------------------------------");
+
+// Exponenciação (**)
+const expo = 5;
+console.log(salary ** expo);
+console.log("--------------------------------");
+
+// => Operadores de comparação
