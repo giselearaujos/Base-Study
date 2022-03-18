61ce10b24ea5c470e9dab1a2ff395eda19caf4b9
giselearaujos <giselearaujo07.s@gmail.com>
2022-03-17 21:01:59 -0300

    :wrench: Add new exercise in javascript file

---
+++ Javascript/005-exercicio/005-exercicio.js
@@ -0,0 +1,8 @@
+alert("Vamos fazer uma soma!!");
+
+const numberOne = prompt("Insira o primeiro número da soma:");
+const numberTwo = prompt("Insira o segundo número da soma:");
+
+const sum = Number(numberOne) + Number(numberTwo);
+
+confirm(`A soma de ${numberOne} + ${numberTwo} é igual a ${sum}`);
