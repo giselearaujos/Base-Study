d834c4ac7f44f1a405fda217aeef1ff1354ed526
giselearaujos <giselearaujo07.s@gmail.com>
2022-03-18 22:19:22 -0300

    :memo: add new file about string

---
+++ Javascript/007-strings.js
@@ -0,0 +1,46 @@
+// Methods String
+
+// charAt - retorna uma string
+let string01 = "Batman";
+console.log(string01.charAt(0)); // B
+
+// charCodeAt
+let string02 = "Superman";
+console.log(string02.charCodeAt(0)); // 83
+
+// concat
+let string03 = "Spider";
+let string04 = "Man";
+console.log(string03.concat(" ", string04));
+
+// length - mostra o comprimento de uma string
+let string05 = "Mulher Maravilha";
+console.log(string05.length); // 16
+
+// match
+
+// replace - retorna uma nova string
+let string06 = "Front-end is the best!";
+console.log(string06.replace("best", "loko")); // Front-end is the loko
+
+// search
+
+// split - método divide uma string
+let string07 = "World,is,big!";
+console.log(string07.split(",")); // [ 'World', 'is', 'big!' ]
+
+// substring
+
+// toUpperCase - converte string em maiusculo
+let string08 = "martin luther king";
+console.log(string08.toLocaleUpperCase()); // MARTIN LUTHER KING
+
+// toLowerCase - converte string em minusculo
+let string09 = "PLaTÃO";
+console.log(string09.toLowerCase()); // platão
+
+// trim - remove espaçoes em brancos de ambas as extremidades
+let string10 = "  Capitão América   ";
+console.log(string10.trim()); // Capitão América
+
+// valueOf
