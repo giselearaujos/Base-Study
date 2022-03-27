1d659cb9e1a3326139e4fba87f4f7d17221e93ce
giselearaujos <giselearaujo07.s@gmail.com>
2022-03-26 22:05:18 -0300

    :seedling: Add new file about arrays in javascript

---
+++ Javascript/012-arrays.js
@@ -0,0 +1,8 @@
+/* -------- Arrays ----------- */
+
+/*
+Array é uma estrutura de dados que armazena uma coleção de elementos
+(valores ou variáveis), cada elemento do array é identificado por um index.
+Em Javascript, os Arrays são objetos que vêm com uma série de métodos 
+embutidos para realizar operações de travessia e mutação.
+*/
