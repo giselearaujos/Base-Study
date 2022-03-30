0b3618c05efb792716f0283979967caca6fe94ce
giselearaujos <giselearaujo07.s@gmail.com>
2022-03-29 22:16:37 -0300

    :construction: Work in progress

--- Javascript/013-function.js
+++ Javascript/013-function.js
@@ -1,1 +1,8 @@
 // -------------- Function ------------------
+// Funções são blocos de instruções que realizam tarefas específicas
+
+function nomeDaFuncao(parametros) {
+  // instruções ...
+  return "valor";
+}
+console.log(nomeDaFuncao());
