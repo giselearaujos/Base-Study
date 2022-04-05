1b850109964cb7b6bf9c81c6e13f1cd26f2cab63
giselearaujos <giselearaujo07.s@gmail.com>
2022-04-04 21:52:15 -0300

    :seedling: Add new function examples

--- Javascript/013-function.js
+++ Javascript/013-function.js
@@ -6,9 +6,39 @@ function nomeDaFuncao(parametros) {
   return "valor";
 }
 console.log(nomeDaFuncao());
+console.log("----------------------------");
 
 function welcome(yourName) {
-  console.log(`Welcome ${yourName}!`);
+  return `Welcome ${yourName}!`;
 }
-welcome("Jaqueline");
-welcome("Vitoria");
+console.log(welcome("Jaqueline"));
+const initial = welcome("José");
+console.log(initial);
+console.log("----------------------------");
+
+function sum(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+console.log(sum(9, 7));
+console.log(sum(3, 8));
+console.log("----------------------------");
+
+// Função anônima
+const square_root = function (number) {
+  return number ** 0.5;
+};
+console.log(square_root(9));
+console.log(square_root(16));
+console.log(square_root(25));
+console.log("----------------------------");
+
+// Arrow Function
+const sub = (number1, number2) => {
+  const result = number1 - number2;
+  return result;
+};
+console.log(sub(9, 3));
+console.log(sub(16, 6));
+console.log(sub(25, 4));
+console.log("----------------------------");
