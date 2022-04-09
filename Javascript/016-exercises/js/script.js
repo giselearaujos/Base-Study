b9f33de0b59a5f0074b298515f1dc768f83a7190
giselearaujos <giselearaujo07.s@gmail.com>
2022-04-09 19:31:35 -0300

    :construction: develop javascript exercise

--- Javascript/016-exercises/js/script.js
+++ Javascript/016-exercises/js/script.js
@@ -1,1 +1,29 @@
+function myScope() {
+  const form = document.querySelector("#form");
+  const result = document.querySelector("#result");
 
+  const people = [];
+
+  // Exemplo 02
+  function sendForm(event) {
+    event.preventDefault();
+
+    const name = form.querySelector("#name");
+    const lastName = form.querySelector("#lastName").value;
+    const weight = form.querySelector("#weight").value;
+    const height = form.querySelector("#height").value;
+
+    // people.push({
+    //   name: name,
+    //   lastName: lastName,
+    //   weight: weight,
+    //   height: height,
+    // });
+    console.log(name.value);
+
+    // result.innerHTML += `<h3>${name.value}</h3>`;
+  }
+
+  form.addEventListener("submit", sendForm);
+}
+myScope();
