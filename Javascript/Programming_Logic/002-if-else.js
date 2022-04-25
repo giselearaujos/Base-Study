10cfd4652b3fbf2d300443488687c4b18f9784a0
giselearaujos <giselearaujo07.s@gmail.com>
2022-04-25 16:51:36 -0300

    :recycle: Refactor code about if and else

--- Javascript/Programming_Logic/002-if-else.js
+++ Javascript/Programming_Logic/002-if-else.js
@@ -6,12 +6,27 @@
    da interação do usuário.
 */
 
-if (condicao) {
-  // declaracao 1, executada quando a condição for verdadeira
+// if (condicao) {
+//    declaracao 1, executada quando a condição for verdadeira
+// } else {
+//    declaracao 2, executada quando a condição for false
+// }
+
+const age = 2;
+if (age >= 18) {
+  console.log(`Your age is ${age} years. You can get a driver's license.`);
 } else {
-  // declaracao 2, executada quando a condição for false
+  console.log(`Your age is ${age} years. You can´t get a driver's license.`);
 }
 
-// cont...
-//
-// Exemplo
+console.log("-----------------------");
+
+const note = 2;
+
+if (note >= 7) {
+  console.log(`Okay!`);
+} else if (note >= 5) {
+  console.log(`Recovery!`);
+} else {
+  console.log(`Disapproved!`);
+}
